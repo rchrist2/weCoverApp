1163e377204fc6bb42bc361b066e34001d471525
rchrist2 <rachelc504@gmail.com>
2020-07-30 09:40:15 -0500

    added links and tried to fix styling for price

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   resources :quotes
   resources :ex_models
   resources :risk_by_brands
+  resources :pages do 
+    collection do
+      get :home
+    end
+  end
+
+  get "app\views\pages\account.html.erb", to: "pages#account", as: "account"
+
   root to: 'pages#home'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
